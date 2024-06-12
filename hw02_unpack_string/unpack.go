c77f6bc527f5d08fb51a1a9036c8a003e6651b45
Ura Kazimirskiy <58485439+KaziPHone@users.noreply.github.com>
2024-06-12 23:06:20 +0300

    add hw 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(v string) (string, error) {
+	arrV := []rune(v)
+	switch {
+	case len(arrV) == 0:
+		return "", nil
+	case !validate(arrV):
+		return "", ErrInvalidString
+	default:
+		return buildString(arrV), nil
+	}
+}
+
+func buildString(arrV []rune) string {
+	result := ""
+	for i, ch := range arrV {
+		switch {
+		case i == len(arrV)-1 && unicode.IsLetter(arrV[i]):
+			result += string(ch)
+		case i < len(arrV)-1 && unicode.IsLetter(arrV[i]) && unicode.IsDigit(arrV[i+1]):
+			result += multiRune(arrV[i], arrV[i+1])
+		case i < len(arrV)-1 && unicode.IsLetter(arrV[i]) && unicode.IsLetter(arrV[i+1]):
+			result += string(ch)
+		case i < len(arrV)-1 && unicode.IsLetter(arrV[i]):
+			continue
+		}
+	}
+	return result
+}
+
+func multiRune(ch, c rune) string {
+	result := ""
+	count, _ := strconv.Atoi(string(c))
+	for i := 0; i < count; i++ {
+		result += string(ch)
+	}
+	return result
+
+}
+
+func validate(v []rune) bool {
+	if (len(v) == 1 && unicode.IsDigit(v[0])) || unicode.IsDigit(v[0]) {
+		return false
+	}
+	for i := range v {
+		if i < len(v)-1 && unicode.IsDigit(v[i]) && unicode.IsDigit(v[i+1]) {
+			return false
+		}
+	}
+	return true
 }
